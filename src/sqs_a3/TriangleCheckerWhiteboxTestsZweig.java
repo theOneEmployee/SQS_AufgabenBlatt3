eea5446c2a8d391f44a991d2ec4a9fd4373d8584
fabia <fabia@DESKTOP-IAIT2Q7.fritz.box>
2021-04-30 17:10:24 +0200

    Zweigüberdeckung

---
+++ src/sqs_a3/TriangleCheckerWhiteboxTestsZweig.java
@@ -0,0 +1,44 @@
+package sqs_a3;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TriangleCheckerWhiteboxTestsZweig {
+	
+	@Test
+	public void checkNoTriangleTestFailureAndSucess()
+	{
+		TriangleType noTriangle = TriangleChecker.checkTriangle(2, 2, 10);
+		assertEquals(TriangleType.NO_TRIANGLE, noTriangle);
+		noTriangle = TriangleChecker.checkTriangle(2, 9, 10);
+		assertNotEquals(TriangleType.NO_TRIANGLE, noTriangle);
+	}
+	
+	@Test
+	public void checkScaleneTriangleTestFailureAndSucess() {
+		TriangleType scaleneTriangle = TriangleChecker.checkTriangle(3, 3, 4);
+		assertNotEquals(TriangleType.SCALENE_TRIANGLE, scaleneTriangle);
+		scaleneTriangle = TriangleChecker.checkTriangle(2, 3, 4);
+		assertEquals(TriangleType.SCALENE_TRIANGLE, scaleneTriangle);
+	}
+	
+	@Test
+	public void checkIsoscelesTriangleTestFailureAndSucess()
+	{
+		TriangleType isoscelesTriangle = TriangleChecker.checkTriangle(3, 4, 5);
+		assertNotEquals(TriangleType.ISOSCELES_TRIANGLE, isoscelesTriangle);
+		isoscelesTriangle = TriangleChecker.checkTriangle(4, 4, 5);
+		assertEquals(TriangleType.ISOSCELES_TRIANGLE, isoscelesTriangle);
+	}
+	
+	@Test
+	public void checkEquilateralTriangleTestFailureAndSucess()
+	{
+		TriangleType equilateralTriangle = TriangleChecker.checkTriangle(2, 3, 2);
+		assertNotEquals(TriangleType.EQUILATERAL_TRIANGLE, equilateralTriangle);
+		equilateralTriangle = TriangleChecker.checkTriangle(1, 1, 1);
+		assertEquals(TriangleType.EQUILATERAL_TRIANGLE, equilateralTriangle);
+	}
+
+}
